dcbf4a105ca084410406da8424918bc94f5a0b50
vinhuyen <vinhuyen.theonly@gmail.com>
2022-05-28 09:51:28 +0700

    change sth

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import {initializeApp} from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCJwHrcsAd9E3l0rLxR1cn0B4FqmA1m_AA",
+    authDomain: "midterm-uyen.firebaseapp.com",
+    projectId: "midterm-uyen",
+    storageBucket: "midterm-uyen.appspot.com",
+    messagingSenderId: "1063353193011",
+    appId: "1:1063353193011:web:1491c3bc6a80ba1fa68946",
+    measurementId: "G-Z6NT17PMF1"
+};
+
+const app = initializeApp(firebaseConfig)
